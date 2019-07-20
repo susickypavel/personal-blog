f8631dc43ae2c993319987860d01083b50b42aa0
Pavel Sušický <pavels.sus@gmail.com>
2019-07-20 17:10:04 +0200

    feat: 🎸 add emotion as CSS-in-JS solution
    add emotion and created global and reset css files, which are placed
    inside Layout component using Global component from emotion

---
+++ src/css/global-styles.ts
@@ -0,0 +1,13 @@
+import { css } from "@emotion/core"
+
+export default css`
+  box-sizing: border-box;
+
+  html,
+  body,
+  #___gatsby,
+  #___gatsby > * {
+    width: 100%;
+    height: 100%;
+  }
+`

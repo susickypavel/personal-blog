940090a409938fb0f5443ebf7d5069e29df9a239
Pavel Sušický <pavels.sus@gmail.com>
2019-08-03 14:03:11 +0200

    fix: 🐛 fix bug when window wasn't defined in certain scenarios

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,3 @@
 export const formatDate = (date: string) =>
-  new Date(date).toLocaleDateString(window ? window.navigator.language : "en-EN")
+  new Date(date).toLocaleDateString(typeof window !== "undefined" ? window.navigator.language : "en-EN")
 export const createPostPath = (id: string) => `/post/${id}`
